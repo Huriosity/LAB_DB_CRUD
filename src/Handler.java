de5b42175af5d255b63253de32d6dbfccef5b10c
Huriosity <sasha_gur1999@mail.ru>
2020-03-07 01:36:59 +0300

    feat: complite configLoader and Server classes, start Hadler class

--- src/Handler.java
+++ src/Handler.java
@@ -1,2 +1,13 @@
-public class Handler {
+import java.net.Socket;
+
+public class Handler extends Thread {
+
+    private Socket socket;
+
+    private String directory;
+
+    Handler(Socket socket, String directory) {
+        this.socket = socket;
+        this.directory = directory;
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,30 @@
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class Server {
+    private int port;
+    private String directory;
+
+    public Server(int port, String directory) {
+        this.port = port;
+        this.directory = directory;
+    }
+
+    void start() {
+        try (var server = new ServerSocket(this.port)) {
+            while (true) {
+                var socket = server.accept();
+                var thread = new Handler(socket, this.directory);
+                thread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+          ConfigLoader.loadConfigENV("src\\resources\\config.properties");
+          new Server(Integer.parseInt(System.getenv().get("port")), System.getenv().get("directory")).start();
+
+    }
 }
